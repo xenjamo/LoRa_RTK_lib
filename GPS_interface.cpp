2546ff0c9b1a073f3a48ee9efbaed746fc2d040f
clemo <xenjamo@gmail.com>
2022-10-27 14:41:30 +0200

    basic working lib

--- GPS_interface.cpp
+++ GPS_interface.cpp
@@ -1,6 +1,7 @@
 
 
 #include "GPS_interface.h"
+#include <cstdint>
 
 /*
  * Copyright (c) 2020 Arm Limited and affiliates.
@@ -10,16 +11,19 @@
 // Create a DigitalOutput object to toggle an LED whenever data is received.
 
 // Create a UnbufferedSerial object with a default baud rate.
-//static UnbufferedSerial serial_port(PA_0, PA_1);
+//static UnbufferedSerial serial_port(PA_0, PA_1, 921600);
 
 RTCM3_UBLOX::RTCM3_UBLOX(UnbufferedSerial *uart){
 
-    _serial_port = uart;
+    _serial_port = uart; //pointer magic
+    
 }
 
 
-//////// nested class block ///////////
-RTCM3_UBLOX::RTCM_MSG::RTCM_MSG(){
+//////// class block ///////////
+
+
+RTCM_MSG::RTCM_MSG(){
         preamble = 0;
         msg_length = 0;
         current_msg_pos = 0;
@@ -28,15 +32,32 @@ RTCM3_UBLOX::RTCM_MSG::RTCM_MSG(){
         msg_valid = 0;
 }
 
-bool RTCM3_UBLOX::RTCM_MSG::checkCRC(){
+bool RTCM_MSG::checkCRC(){
 
     return 0;
 }
-bool RTCM3_UBLOX::RTCM_MSG::write2array(uint8_t *buf, uint8_t *len){
+
+
+// writes all its contents to an array and clears itself for next msg
+
+bool RTCM_MSG::write2array(uint8_t *buf, uint16_t &len){
+    len = msg_length + 6;
+    buf[0] = preamble;
+    buf[1] = (uint8_t)(msg_length >> 8);
+    buf[2] = (uint8_t) msg_length      ;
+
+    for(int i = 0; i < msg_length; i++){
+        buf[3+i] = data[i];
+    }
+    buf[3+msg_length]   = (uint8_t)(crc >> 16);
+    buf[3+msg_length+1] = (uint8_t)(crc >>  8);
+    buf[3+msg_length+2] = (uint8_t) crc       ;
+
+    clearMsg();
 
     return 0;
 }
-bool RTCM3_UBLOX::RTCM_MSG::clearMsg(){
+bool RTCM_MSG::clearMsg(){
 
     preamble = 0;
     msg_length = 0;
@@ -48,79 +69,123 @@ bool RTCM3_UBLOX::RTCM_MSG::clearMsg(){
     return 0;
 }
 
-//////// end of nested class block ///////////
+//////// end of class block ///////////
 
-bool RTCM3_UBLOX::init_(){
-    clearAll();
+bool RTCM3_UBLOX::init(){
+    clearAll(); //reset everythin to make sure all is set to 0
     current_msg = 0;
-    msg_pos = PREAMBLE; // make int ready to recieve stuff
+    msg_pos = MSG_PREAMBLE; // make interrupt ready to recieve stuff
     c = 0; //clear buffer
+    //_serial_port->format(8,SerialBase::None,1);
+    _serial_port->attach(callback(this, &RTCM3_UBLOX::rx_interrupt_handler), SerialBase::RxIrq); //start isr
     return 0;
+}
+
+
+//indicates if RTCM msg hase been recieved and outputs the number uf messages it has stored
+
+uint8_t RTCM3_UBLOX::msg_ready(){
+    if(msg[current_msg].msg_valid){
+        return current_msg+1;
+    }else{
+        return 0;
+    }
+}
+
+uint8_t RTCM3_UBLOX::readMsg(uint8_t n, uint8_t *buf, uint16_t &len){
+    if(!msg[n].msg_valid | (n > MAXIMUM_MESSAGES)){
+        return 0;
+    }
+
+    msg[n].write2array(buf, len);
+    
+    if(n+1 > MAXIMUM_MESSAGES){
+        //check if new msg
+        if(msg[n+1].msg_valid | msg[n+1].msg_incoming){
+            printf("a newer message just came in\n");
+            return 2;
+        }
+    }
+    
+    return 1;
+
 }
 
 
 bool RTCM3_UBLOX::clearAll(){
     for(int i = 0; i < MAXIMUM_MESSAGES; i++){
         msg[i].clearMsg();
-        
     }
+    current_msg = 0;
+    reached_max_msg = 0;
     return 1;
 }
 
 
+
+
+// private
 void RTCM3_UBLOX::rx_interrupt_handler()
 {
+    
     uint8_t i = current_msg;
-
+    
     _serial_port->read(&c,1);
-    if(msg[0].msg_valid){
-        i++;
-        current_msg++;
-    }
+    
 
     switch (msg_pos){
-        case(PREAMBLE):
+        case(MSG_PREAMBLE):
             if(c == 0xd3){
                 //preamble detected
+                msg[i].msg_incoming = true;
                 msg_pos = MSG_LENGTH;
-                msg[0].preamble = c;
-                msg[0].current_msg_pos = 1;
+                msg[i].preamble = c;
+                msg[i].current_msg_pos = 1;
             }
         break;
         case(MSG_LENGTH):
             //read msg length
-            msg[0].msg_length = msg[0].msg_length | (((uint16_t)c) << 8*msg[0].current_msg_pos);
+            msg[i].msg_length = msg[i].msg_length | (((uint16_t)c) << 8*msg[i].current_msg_pos);
 
-            if(msg[0].current_msg_pos == 0){
+            if(msg[i].current_msg_pos == 0){
                 //LSB
-                msg_pos = DATA;
+                msg_pos = MSG_DATA;
 
             } else {
-                msg[0].current_msg_pos--;
+                msg[i].current_msg_pos--;
             }
 
         break;
-        case(DATA):
+        case(MSG_DATA):
             //read data
-            msg[0].data[msg[0].current_msg_pos] = c;
-            msg[0].current_msg_pos++;
-            if(msg[0].current_msg_pos == msg[0].msg_length){
-                msg[0].current_msg_pos = 2;
-                msg_pos = CRC_;
+            msg[i].data[msg[i].current_msg_pos] = c;
+            msg[i].current_msg_pos++;
+            if(msg[i].current_msg_pos == msg[i].msg_length){
+                msg[i].current_msg_pos = 2;
+                msg_pos = MSG_CRC;
             }
 
         break;
-        case(CRC_):
+        case(MSG_CRC):
             //read crc
-            msg[0].crc = msg[0].crc | (((uint32_t)c) << 8*msg[0].current_msg_pos);
+            msg[i].crc = msg[i].crc | (((uint32_t)c) << 8*msg[i].current_msg_pos);
 
-            if(msg[0].current_msg_pos == 0){
+            if(msg[i].current_msg_pos == 0){
                 //LSB
-                msg_pos = PREAMBLE;
-                msg[0].msg_valid = true;
+                msg_pos = MSG_PREAMBLE;
+                msg[i].msg_incoming = false;
+                msg[i].msg_valid = true;
+                
+                if(current_msg+1 >= MAXIMUM_MESSAGES){
+                    reached_max_msg = true;
+                }else{
+                    current_msg++;
+                }
+
+                
 
             } else {
-                msg[0].current_msg_pos--;
+                msg[i].current_msg_pos--;
             }
 
         break;
@@ -133,12 +198,7 @@ void RTCM3_UBLOX::rx_interrupt_handler()
 }
 
 
-void print_hex(const char *s, int len)
-{
-    while(len--){
-        printf("%02x", (unsigned int) *s++);
-    }
-}
+
 
 void clear_buf(uint8_t *buf, int length){
     for(int i = 0; i<length; i++){
--- GPS_interface.h
+++ GPS_interface.h
@@ -5,52 +5,59 @@
 
 
 #define MAXIMUM_BUFFER_SIZE     1+2+1023+3 //preamble + length + maxdata + crc
-#define MAXIMUM_MESSAGES        10
+#define MAXIMUM_MESSAGES        4           //MUST be the same size as expected messages
 
 
 
 typedef enum{
-    IDLE,
-	PREAMBLE,
+    MSG_IDLE,
+	MSG_PREAMBLE,
 	MSG_LENGTH,
-    DATA,
-    CRC_,
+    MSG_DATA,
+    MSG_CRC,
     MSG_COMPLETE,
     MSG_ERR
 }msg_pos_t;
+
+class RTCM_MSG{
+
+    public:
+    RTCM_MSG();
+    uint8_t preamble;
+    uint16_t msg_length;
+    uint16_t current_msg_pos;
+    uint8_t data[MAXIMUM_BUFFER_SIZE];
+    uint32_t crc; //accually its 24bits
+    bool crc_valid;
+    bool msg_incoming;
+    bool msg_valid;
+    bool checkCRC();
+    bool write2array(uint8_t *buf, uint16_t &len);
+    bool clearMsg();
+    private:
+    //nothing private yet
+};
 
 class RTCM3_UBLOX{
     public:
     RTCM3_UBLOX(UnbufferedSerial*);
 
-    class RTCM_MSG{
-        public:
-        RTCM_MSG();
-        uint8_t preamble;
-        uint16_t msg_length;
-        uint16_t current_msg_pos;
-        uint8_t data[MAXIMUM_BUFFER_SIZE];
-        uint32_t crc; //accually its 24bits
-        bool crc_valid;
-        bool msg_valid;
-        bool checkCRC();
-        bool write2array(uint8_t *buf, uint8_t *len);
-        bool clearMsg();
-        private:
-        //nothing private yet
-        };
 
-    bool init_();
-    char c; // most important char ever lol
+    bool init();
     uint8_t current_msg;
     msg_pos_t msg_pos;
+    UnbufferedSerial *_serial_port;
     RTCM_MSG msg[MAXIMUM_MESSAGES];
+    bool reached_max_msg;
+    uint8_t msg_ready();
+    uint8_t readMsg(uint8_t, uint8_t *buf, uint16_t &len);
     bool clearAll();
 
     
     private:
-    UnbufferedSerial *_serial_port;
+    //UnbufferedSerial *_serial_port;
     void rx_interrupt_handler();
+    char c; // most important char ever lol
 
 
 
